20d5aee6c4d7c97a18a7709594dabdc5380aea72
Pratik Nath Tiwari <senpai@Pratiks-MacBook-Air.local>
2021-08-23 13:13:22 +0530

    First commit

---
+++ lib/screens/favourites_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavourtiesScreen extends StatelessWidget {
+  const FavourtiesScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Favourites'),
+    );
+  }
+}
